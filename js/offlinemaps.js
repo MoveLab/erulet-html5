c349c029566dd8af7317249545ec1f31edb176f2
Jordi López <unblockingcoder@gmail.com>
2014-11-21 20:27:45 +0100

    Fixed map behavior

--- js/offlinemaps.js
+++ js/offlinemaps.js
@@ -14,11 +14,22 @@ var sqlite = null;
 var DB = null;
 var metadata = null;
 
+//Map data
+var maxZ = 18;
+var minZ = 0;
+var lat, lon, latt, lonn, compos, str;
+var imgType;
+
+var map;
+var onlineLayer, offlineLayer;
+var baseMaps, baseOverlay;
+var control;
+
 $(document).on('pageshow', '#trip_select', function() {
 //function initMaps() {
     // try to load cached map
     openDB();
-    //dbMap(map);
+    initMap();
 
     $("#dl_r0").click(function(e) {
         if (navigator.onLine) {         // No internet, can't download
@@ -71,10 +82,9 @@ function openDB() {
             console.log("Tables found on the DB: ");
             var result=sqlite.exec("SELECT name FROM sqlite_master WHERE type = 'table'");
             console.table(result[0].values);
-
-            dbMap(map);
+            addDBMap();
         }
-    });
+    });//.then(function(doc) { addDBMap(); } );
 }
 
 function getBundleFile(path) {
@@ -93,101 +103,109 @@ function getBundleFile(path) {
       // contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
 
       DB.put({_id: "bundle", mbtiles:path ,database: uInt8Array}, function(err, response) {
-              if(err) {
-                  if(err.status==500) {
-                      $('#popupDataPresent').popup();
-                      $('#popupDataPresent').popup('open');
-                  }
-                  else {
-                      alert(err);
-                  }
-                  $.mobile.loading("hide");
-                  throw err;
+          if(err) {
+              if(err.status==500) {
+                  $('#popupDataPresent').popup();
+                  $('#popupDataPresent').popup('open');
               }
               else {
-                  //console.log("saved: ", response.id + " rev: " + response.rev);
-                  //fillDB(zip);
-                  $.mobile.loading("hide");
+                  alert(err);
               }
-          });
+              $.mobile.loading("hide");
+              throw err;
+          }
+          else {
+              //console.log("saved: ", response.id + " rev: " + response.rev);
+              //fillDB(zip);
+              $.mobile.loading("hide");
+          }
+      }).then(function(doc) { addDBMap(); });
     };
     xhr.send();
 
 
 }
 
-function dbMap(map) {
-    console.log(OFFMAP_NAME + ": dbMap()");
-    var maxZ = 11;
-    var minZ = 2;
-    var lat, lon, latt, lonn, compos, str;
-    var imgType;
-    if(sqlite!=null)  {
-        maxZ = sqlite.exec("SELECT max(zoom_level) FROM tiles")[0].values[0];
-        minZ = sqlite.exec("SELECT min(zoom_level) FROM tiles")[0].values[0];
-
-
-        metadata = sqlite.exec("SELECT * FROM metadata");
-        imgType = metadata[0].values[5][1];
-        str = metadata[0].values[0][1];
-        compos = str.indexOf(',');
-        lon = parseFloat(str.substring(0, compos));
-        str = str.substring(compos+1, str.length);
-        compos = str.indexOf(',');
-        lat = parseFloat(str.substring(0, compos));
-        str = str.substring(compos+1, str.length);
-        compos = str.indexOf(',');
-        lonn = parseFloat(str.substring(0, compos));
-        str = str.substring(compos+1, str.length);
-        latt = parseFloat(str.substring(0, str.length));
-        console.log(OFFMAP_NAME + ": maxZoom is " + maxZ + " and minZoom is " + minZ);
-    var onlineLayer = L.tileLayer(
+function initMap() {
+    console.log(OFFMAP_NAME + ": initMap()");
+    onlineLayer = L.tileLayer(
         'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
         {attribution: "\u00a9 <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"}
     );
-   var offlineLayer = new L.TileLayer.Functional(function(view) {
-        var deferred = new jQuery.Deferred();
-        var id = view.zoom + "/" + view.tile.column + "/" + view.tile.row + "." + imgType;
-        //console.log(id);
-        var imgBlob = sqlite.exec("SELECT tile_data FROM tiles WHERE zoom_level="+view.zoom+" AND tile_column="+view.tile.column+" AND tile_row="+view.tile.row);
-        if(!imgBlob[0]) {
-            //console.warn("Couldn't find tile " + id);
-        }
-        else {
-            //console.warn("Found tile " + id);
-            var reader = new FileReader();
-            reader.addEventListener("loadend", function() {
 
-                //console.log(reader.result);
-                var imgURL = reader.result;
-                deferred.resolve(imgURL);
-
-                //URL.revokeObjectURL(imgURL);
-            });
-            //console.log(imgBlob[0].values[0][0]);
-            reader.readAsDataURL(new Blob([imgBlob[0].values[0][0]], {type:"image/"+imgType}));
-
-        }
-        return deferred.promise();
-    }, {tms: true});
-
-    var map = L.map('map',{
+    map = L.map('map',{
         maxZoom: maxZ,
         minZoom: minZ,
-        //maxBounds: [{lat:lat, lon:lon}, {lat:latt, lon:lonn}],
-        layers: [offlineLayer, onlineLayer],
         zoom: maxZ-4,
+        layers: [onlineLayer],
         center: [0.802911, 42.707002]
     }).setView(new L.LatLng(42.707002, 0.802911), maxZ-4);
 
-    var baseMaps = {
-        "Offline": offlineLayer,
+    baseMaps = {
         "Online": onlineLayer
     };
 
-    L.control.layers(baseMaps).addTo(map);
-    L.grid().addTo(map);
-    }
+    baseOverlay = {
+        "Grid": L.grid()
+    };
+    control = L.control.layers(baseMaps, baseOverlay);
+    control.addTo(map);
+}
+
+function addDBMap() {
+    console.log(OFFMAP_NAME + ": addDBMap()");
+    maxZ = sqlite.exec("SELECT max(zoom_level) FROM tiles")[0].values[0];
+    minZ = sqlite.exec("SELECT min(zoom_level) FROM tiles")[0].values[0];
+
+
+    metadata = sqlite.exec("SELECT * FROM metadata");
+    imgType = metadata[0].values[5][1];
+    str = metadata[0].values[0][1];
+    compos = str.indexOf(',');
+    lon = parseFloat(str.substring(0, compos));
+    str = str.substring(compos+1, str.length);
+    compos = str.indexOf(',');
+    lat = parseFloat(str.substring(0, compos));
+    str = str.substring(compos+1, str.length);
+    compos = str.indexOf(',');
+    lonn = parseFloat(str.substring(0, compos));
+    str = str.substring(compos+1, str.length);
+    latt = parseFloat(str.substring(0, str.length));
+    console.log(OFFMAP_NAME + ": maxZoom is " + maxZ + " and minZoom is " + minZ);
+    offlineLayer = new L.TileLayer.Functional(function(view) {
+            var deferred = new jQuery.Deferred();
+            var id = view.zoom + "/" + view.tile.column + "/" + view.tile.row + "." + imgType;
+            //console.log(id);
+            var imgBlob = sqlite.exec("SELECT tile_data FROM tiles WHERE zoom_level="+view.zoom+" AND tile_column="+view.tile.column+" AND tile_row="+view.tile.row);
+            if(!imgBlob[0]) {
+                //console.warn("Couldn't find tile " + id);
+            }
+            else {
+                //console.warn("Found tile " + id);
+                var reader = new FileReader();
+                reader.addEventListener("loadend", function() {
+
+                    //console.log(reader.result);
+                    var imgURL = reader.result;
+                    deferred.resolve(imgURL);
+
+                    //URL.revokeObjectURL(imgURL);
+                });
+                //console.log(imgBlob[0].values[0][0]);
+                reader.readAsDataURL(new Blob([imgBlob[0].values[0][0]], {type:"image/"+imgType}));
+
+            }
+            return deferred.promise();
+    }, {tms: true});
+
+    map.removeControl(control);
+    offlineLayer.setZIndex(-1);
+    baseMaps = {
+        "Offline": offlineLayer,
+        "Online": onlineLayer
+    };
+    control = L.control.layers(baseMaps, baseOverlay);
+    control.addTo(map);
 }
 
 function deleteDB() {
