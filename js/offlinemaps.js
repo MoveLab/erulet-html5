f9fa76775452ef844fb8531d26586995a3f3a4e7
Jordi López <unblockingcoder@gmail.com>
2014-11-23 23:38:51 +0100

    (Finally) brought order to chaos. Fixed CORS and navigation issues with trip_select. Moved some code from utils.js to offlinemaps.js, i think it's easier to read now. Started working on showing routes on map.

--- js/offlinemaps.js
+++ js/offlinemaps.js
@@ -26,31 +26,81 @@ var onlineLayer, offlineLayer;
 var baseMaps, baseOverlay;
 var control;
 
-$(document).on('pagechange', function() {
+$(document).on('pagebeforeshow', function() {   // Handle UI changes
 
     // Make sure controls are visible
     $("#routeControls").show("slow");
 
 
-    // try to load cached map
-    openDB();
-
-    initMap();
-
-});
-
-$(document).on('pageinit', '#trip_select', function() {
-
-
     // Handle clear button
     $("#dl_clear").click(function(e) {
-        e.preventDefault(); // it's an anchor
+        //e.preventDefault(); // it's an anchor
        deleteDB();
        $.mobile.navigate("#switchboard");
     });
 
 
+
+    $("#routeView").click(function(e) {
+
+       // Hide controls
+       $("#routeControls").hide("slow");
+
+       // Let's make the map use the whole space
+       var windowHeight = $(window).height();
+       var headerHeight = $("#mapHeader").height();
+
+       // Expected height is window - header
+       var realHeight = windowHeight - headerHeight;
+
+       $("#map").height(realHeight);
+       $("#map").css({ top: headerHeight }); // position under header
+       removeMarkers();
+
+       if(routesData) {
+            var steps = routesData[localStorage.getItem("selectedRoute")].track.steps;
+            var latlngs = [];
+            console.log(steps);
+            $(steps).each(function(index, value) {
+                if(value.order!=null) {
+                    latlngs[latlngs.length] = new L.LatLng(value.latitude, value.longitude);
+                }
+            });
+            //console.log(latlngs);
+            latlngs.sort(function(a,b){return a.order-b.order});
+            var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+            map.fitBounds(polyline.getBounds());
+       }
+    });
+
+    $("#routeSelect").click(function(e) {
+
+        if (navigator.onLine) {         // No internet, can't download
+            $.mobile.loading("show", {
+                text: "downloading files",
+                textVisible: true,
+                theme: "b",
+                html: ""
+            });
+           var filename = $(this).data('mapid');
+           //We'll use zip to avoid 5 mb quota limit of localStorage
+           //filename = filename.replace('.mbtiles', '.mbtiles.zip');
+           //console.log("Map file to load: " + $("#dl_r1").data('mapid'));
+           getBundleFile(filename);
+       }
+       else {
+            $('#popupNoInternet').popup();
+            $('#popupNoInternet').popup('open');
+       }
+    });
+
+
 });
+
+$(document).ready(function() {
+    loadRoutes();
+});
+
 
 $(document).on('pageshow', '#trip_select', function() {
     // Workaround to show loading dialog (can't be used on document.ready()
@@ -240,5 +290,85 @@ function deleteDB() {
            console.log("Database deleted");
            manifest = null;
        }
+    });
+}
+
+function openRouteDescription(mapID, arrayPosition) {
+    // Define message
+    $("#routeDescription").html(routesData[arrayPosition]["description_"+lang]);
+
+    // Define mapid
+    $("#routeSelect").data('mapid', mapID);
+
+    $("#popupRoute").popup();
+    $("#popupRoute").popup('open');
+}
+
+function setUpButtons() {
+
+    $("#selectRoutes").change(function() {
+        if($("#selectRoutes :selected").text()!="") {
+            var mapID = $("#selectRoutes :selected").data('mapid');
+            var position = $("#selectRoutes").val()-1;
+            openRouteDescription(mapID, position);
+        }
+    });
+}
+
+
+function loadRoutes() {
+    console.log("Retrieving routes...");
+    // $.mobile.loading moved to 'pageshow' (does not work here)
+    $.ajax({
+        type: "GET",
+        url: HOLETSERVER_APIURL + HOLETSERVER_APIURL_ROUTES,
+        crossDomain: true,
+        withCredentials: true,
+        //headers: { Authorization: HOLETSERVER_AUTHORIZATION },
+        beforeSend: function(request) {
+            request.setRequestHeader("Authorization", HOLETSERVER_AUTHORIZATION);
+        },
+        success: function(data) {
+            var count = data.length;
+            //console.log("Fetched " + count + " elements: " );
+            //console.table(data, ["server_id", "name_ca", "name_es", "map.map_file_name"]);
+            //console.table(data  );
+            $(data).each(function(index, value) {
+                $("#selectRoutes").append("<option value='"+(index+1)+"' data-mapid='"+ value.map.map_file_name +"'>"+value["name_"+lang]+"</option");
+
+                    // Create an element to hold all your text and markup
+                    var container = $('<div />');
+
+                    // Delegate all event handling for the container itself and its contents to the container
+                    container.on('click', '.mapPopupLink', function() {
+                        var mapID = $(this).data('mapid');
+                        var position = $(this).data('arraypos');
+                        openRouteDescription(mapID, position);
+                    });
+
+                    // Insert whatever you want into the container, using whichever approach you prefer
+                    container.html("<b><a class='mapPopupLink' href='#' data-arraypos='" + index + "' data-mapid='"  + value.map.map_file_name + "'>" + value["name_"+lang] + "</a></b></br><p>" + value.track.steps[0].latitude + ","+ value.track.steps[0].longitude+ "</p>");
+                    //container.append($('<span class="bold">').text(" :)"));
+
+                    // Show routes on the map
+                    addMarkerWithPopup(new L.LatLng(value.track.steps[0].latitude, value.track.steps[0].longitude), container[0]);
+
+                    localStorage.setItem("selectedRoute", index);
+
+            });
+            routesData = data;
+        },
+        error: function(error) {
+            console.log(error);
+            alert("ERROR: Probably a network (offline/CORS) issue");
+            $.mobile.loading("hide");
+        }
+
+    }).done( function() {
+        $.mobile.loading("hide");
+        initMap();
+        setUpButtons();
+    }).done (function() {
+        openDB();
     });
 }
